542ebf3db53ad97a765c78e7e40616103523a6e6
Tyler Panek <tyler.panek@jacks.sdstate.edu>
2017-11-06 21:02:07 -0600

    Removed GPIO header, because FUNctions are funner. Put FUNction in main file

--- Objects/main.d
+++ Objects/main.d
@@ -6,7 +6,6 @@
 .\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\cmsis_compiler.h
 .\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\cmsis_armcc.h
 .\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.11.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
-.\objects\main.o: ConfigGPIO.h
 .\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.11.0\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h
 .\objects\main.o: .\RTE\Device\STM32F407VGTx\stm32f4xx_hal_conf.h
 .\objects\main.o: .\RTE\_Target_1\RTE_Components.h
